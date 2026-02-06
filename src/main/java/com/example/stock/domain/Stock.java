ecb68624e7e4f7ce1ff4230f5b12e88f0e3a6690
휘래 김 <hramst0618@gmail.com>
2026-02-06 14:00:00 +0900

    feat: Optimistic Lock을 통한 해결

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -1,9 +1,6 @@
 package com.example.stock.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
@@ -16,6 +13,9 @@ public class Stock {
 
     @Getter
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock() {
     }
