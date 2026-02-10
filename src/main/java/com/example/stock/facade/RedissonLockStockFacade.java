f6a022e978fc4cc53716fa80e9c1e18785e06d01
휘래 김 <hramst0618@gmail.com>
2026-02-10 14:00:00 +0900

    feat: Redis Redisson을 통한 해결

---
+++ src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,36 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+
+    private final StockService stockService;
+    private final RedissonClient redissonClient;
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.MILLISECONDS);
+
+            if(!available) {
+                System.out.println("Lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
